f057666f49c4cd5d7052fd8f087a6431a89db256
Enrico Gaensler <gaensler@amazon.de>
2023-03-20 11:33:04 +0100

    remove unnecessary command

--- scripts/cp_datasets.sh
+++ scripts/cp_datasets.sh
@@ -2,6 +2,5 @@
 
 cd ../data/
 
-aws s3 cp cur-data-december.csv s3://$1-$2-cur-data/cur-hourly/year=2022/cur-data-december.csv 
 aws s3 cp cur-data-december.parquet s3://$1-$2-cur-data/cur-hourly/year=2022/cur-data-december.parquet 
 aws s3 cp dog-booking-data.csv s3://$1-$2-dog-bookings/december/dog_booking_data.csv 
