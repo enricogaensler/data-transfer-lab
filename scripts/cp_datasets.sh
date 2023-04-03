09ba746195af82cab233257619f61ceefb065355
Enrico Gaensler <gaensler@amazon.de>
2023-04-03 11:59:52 +0200

    correct scripts for partioning

--- scripts/cp_datasets.sh
+++ scripts/cp_datasets.sh
@@ -2,5 +2,5 @@
 
 cd ../data/
 
-aws s3 cp cur-data-december.parquet s3://$1-$2-cur-data/cur-hourly/year=2022/cur-data-december.parquet 
-aws s3 cp dog-booking-data.csv s3://$1-$2-dog-bookings/december/dog_booking_data.csv 
+aws s3 cp cur-data-december.parquet s3://$1-$2-cur-data/cur-hourly/year=2022/month=12/cur-data-december.parquet 
+aws s3 cp dog-booking-data.csv s3://$1-$2-dog-bookings/year=2022/month=12/dog_booking_data.csv 
